ed21971958484a3aeacc3cfdf8cb7a039a97af45
alexmaximovna <alex.maximovna@gmail.com>
2017-12-02 23:13:55 +0600

    recorrect gulp

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var nunjucks = require('gulp-nunjucks');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+var path = {
+     css:  'src/styles/*.scss',
+    html: 'src/templates/*.html',
+    images: 'src/images/*.*',
+    fonts: 'src/fonts/**/*.*',
+    dist: {
+      css:  'dist/styles/',
+      html: 'dist/',
+      images: 'dist/images/',
+      fonts: 'dist/fonts/'
+    }
+};
+gulp.task('default', ['build', 'serve', 'watch']);
+
+gulp.task('images', function () {
+  return gulp.src(path.images)
+    .pipe(gulp.dest(path.dist.images));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(path.fonts)
+    .pipe(gulp.dest(path.dist.fonts));
+});
+
+gulp.task('css', function () {
+  return gulp.src(path.css)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+gulp.task('html', function () {
+  return gulp.src(path.html)
+    .pipe(nunjucks.compile())
+    .pipe(gulp.dest(path.dist.html));
+});
+
+gulp.task('build', ['html', 'css','images', 'fonts']);
+
+gulp.task('watch', function () {
+  gulp.watch(path.images, ['images']);
+  gulp.watch(path.fonts, ['fonts']);
+  gulp.watch(path.css, ['css']);
+  gulp.watch(path.html, ['html']);
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: path.dist.html
+    }
+  });
+  gulp.watch('dist/**').on('change', browserSync.reload);
+});
